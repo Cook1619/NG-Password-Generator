355a043ce0dc30d14b989e2fd595690890b0e58c
Cook1619 <cook6348@gmail.com>
2020-07-23 20:32:44 -0500

    Adding functions to handle wheather check boxes are check or not and handling length prop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password = '';
+  length = 0;
+  includeLetters = false
+  includeNumbers = false
+  includeSymbols = false
 
   onButtonClick() {
     this.password = 'My Password'
+    console.log(length)
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onChangeLength(value: string){
+    const parsedValue = parseInt(value)
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue
+    }
   }
 }
