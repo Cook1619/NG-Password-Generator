700c484b2dde2668c59e42eefd2e62e709769e32
Cook1619 <cook6348@gmail.com>
2020-07-23 20:05:09 -0500

    Using property binding and event binding syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-gen';
+  password = '';
+
+  onButtonClick() {
+    this.password = 'My Password'
+  }
 }
